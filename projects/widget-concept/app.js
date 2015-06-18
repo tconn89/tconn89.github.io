83d5bd5367108a8602ed22dfea9fcf223920d4a2
tconn89 <tconn89@gmail.com>
2015-06-18 17:53:03 -0400

    update widget concept

--- projects/widget-concept/app.js
+++ projects/widget-concept/app.js
@@ -5,6 +5,29 @@ document.documentElement.clientHeight, window.innerHeight || 0);
 
 $('.page').height(h);
 $('.dropzone').data( "filled", false );
+$(document).ready(function() {
+        var  evenmore = '1000px';     
+
+    $('.next').on('click', function(e){
+            e.preventDefault();
+            $('#drag-section').css('margin-left','-' + evenmore);
+            
+            $('.selected').removeClass('selected').addClass('tab').next().removeClass('tab').addClass('selected'); 
+            setTimeout(function(){
+                    $("#client_image_preview").attr("src", imagelocation);
+            }, 800);
+            //change 700px to 1400px go
+               
+    });
+});
+
+
+// Load tabs
+//
+// ---------------------------
+var img = new Image();
+var imagelocation = 'time.png'
+//--------------------------------
 $(function() {
     $( ".draggable" ).draggable({
         revert: true,
@@ -17,8 +40,10 @@ $(function() {
         }
     });
     $('.dropzone').droppable({
-        
         drop: function( event, ui ) {
+
+            var innerText = $(this).children('.priority');
+            innerText.empty().html(ui.draggable.html());
 
             var newId = $('.drug').attr('id');
             if(!$(this).data('filled')){
